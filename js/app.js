9f3e0cbabaf11dc048a8c7fb53045cc92efb8eb2
patmat <patrik.gaertner@gmail.com>
2019-11-09 20:17:22 +0100

    added save function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,26 @@
 const btnRoll = document.querySelector('.btn_roll');
+const btnSave = document.querySelector('.btn_save');
 
 btnRoll.addEventListener('click', () => {
   roll();
 });
+btnSave.addEventListener('click', () => {
+  save();
+});
+
+let randomOne = null;
+let randomTwo = null;
+let randomThree = null;
+let randomFour = null;
+let randomFive = null;
+let randomSix = null;
+
+const diceOneArray = ['01', '02', '03', '04', '05', '06'];
+const diceTwoArray = ['01', '02', '03', '04', '05', '06'];
+const diceThreeArray = ['01', '02', '03', '04', '05', '06'];
+const diceFourArray = ['01', '02', '03', '04', '05', '06'];
+const diceFiveArray = ['01', '02', '03', '04', '05', '06'];
+const diceSixArray = ['01', '02', '03', '04', '05', '06'];
 
 function roll() {
   const dice1 = document.querySelector('.dice1');
@@ -15,7 +33,6 @@ function roll() {
   if (!document.getElementById('dice-1').checked) {
     randomOne = Math.floor(Math.random() * 6) + 1;
     dice1.src = `img/dice-${randomOne}.png`;
-    console.log(!document.getElementById('dice-1').checked);
   }
   if (!document.getElementById('dice-2').checked) {
     randomTwo = Math.floor(Math.random() * 6) + 1;
@@ -38,4 +55,13 @@ function roll() {
     randomSix = Math.floor(Math.random() * 6) + 1;
     dice6.src = `img/dice-${randomSix}.png`;
   }
+}
+
+function save() {
+  console.log(diceOneArray[randomOne - 1]);
+  console.log(diceTwoArray[randomTwo - 1]);
+  console.log(diceThreeArray[randomThree - 1]);
+  console.log(diceFourArray[randomFour - 1]);
+  console.log(diceFiveArray[randomFive - 1]);
+  console.log(diceSixArray[randomSix - 1]);
 }
